2b0e003d485dee011a9b517b7a6769b72cf96783
gonzalo99c <gonzalo-falco@outlook.com.ar>
2021-09-28 20:18:00 -0300

    a ver si se juntan bien las ramas

--- Presentacion/frmRegistrarReserva.cs
+++ Presentacion/frmRegistrarReserva.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using AppBTS.Negocio;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,27 @@ namespace AppBTS.Presentacion
 {
     public partial class frmRegistrarReserva : Form
     {
+        Reservas oReserva = new Reservas();
+        Mesas oMesa = new Mesas();
         public frmRegistrarReserva()
         {
             InitializeComponent();
+        }
+
+        private void frmRegistrarReserva_Load(object sender, EventArgs e)
+        {
+            CargarCombo(cboMesa, oMesa.RecuperarTodos());
+            dtpReserva.Format = DateTimePickerFormat.Custom;
+            dtpReserva.CustomFormat = "dd/MM/yyyy hh:mm";
+        }
+
+        private void CargarCombo(ComboBox combo, DataTable tabla)
+        {
+            combo.DataSource = tabla;
+            combo.DisplayMember = tabla.Columns[0].ColumnName;
+            combo.ValueMember = tabla.Columns[0].ColumnName;
+            combo.SelectedIndex = -1;
+            combo.DropDownStyle = ComboBoxStyle.DropDownList;
         }
     }
 }
